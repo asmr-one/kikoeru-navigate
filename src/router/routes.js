107c7660d50fb5da80dddf70b182c4607c9764b6
Anonymous <>
2023-02-16 17:50:00 +0900

    init

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,7 +9,8 @@ const routes = [
   // but you can also remove it
   {
     path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
+    // component: () => import("pages/ErrorNotFound.vue"),
+    redirect: "/",
   },
 ];
 

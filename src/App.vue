ce633dbe158df7b6ce3775ff04468eededbeb2bf
Anonymous <>
2023-03-05 02:06:18 +0900

    update: 通过其他链接（旧搜索引擎结果）进入导航页的，跳转到根目录

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,34 @@
 
 <script>
 import { defineComponent } from "vue";
+import { useMeta } from 'quasar';
 import MainLayout from "layouts/MainLayout.vue";
+
+
+function redirectIfNotRoot() {
+  if (window.location.pathname === "/" && window.location.search === "")
+    return
+
+  // prerender ua 通过 head 通知，不需要重定向
+  if (window.navigator.userAgent.startsWith('special-ua-for-prerender-')) {
+    useMeta({
+      meta: {
+        "prerender-status-code": { name: "prerender-status-code", content: "301" },
+        "prerender-header": { name: "prerender-header", content: "Location: /" },
+      },
+    })
+  } else {
+    window.location.replace("/")
+  }
+}
 
 export default defineComponent({
   name: "App",
-
-  components: { MainLayout, }
+  components: { MainLayout, },
+  methods: {
+  },
+  setup() {
+    redirectIfNotRoot()
+  }
 });
 </script>
